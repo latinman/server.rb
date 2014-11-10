de0d3d3283693c6c3f994934dc9439bd547cbe79
Ernesto <erivas78@gmail.com>
2014-11-10 15:25:33 -0500

    first commit

--- server.rb
+++ server.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
 
 get '/' do
-	redirect to('/hello_world.html')
+		
+	class Bill
+		attr_reader :amount
+		attr_accessor :tip
+
+		def initialize(amount)
+			@amount = amount
+		end
+
+		def add_tip(tip)
+			@total = (amount * 0.07).to_f.round(2) + tip
+
+		end
+		def view_total
+			@total = view_total
+		end
+	end
+
+	bill = Bill.new(25.55)
+	bill.add_tip(0.07)
+	"#{bill.view_total}"
 end
